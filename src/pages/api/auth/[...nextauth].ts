090f3000b7c9f13624f4dd50d964504ac0c79e9e
cinques <cinquestyle@gmail.com>
2023-10-27 14:24:38 +0300

    prisma adapter

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,14 @@
 import NextAuth from "next-auth";
 import VkProvider from "next-auth/providers/vk";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { db } from "@/server/db";
 
-export const authOptions = {
+export default NextAuth({
   providers: [
     VkProvider({
       clientId: process.env.VK_ID,
       clientSecret: process.env.VK_SECRET,
     }),
   ],
-};
-
-export default NextAuth(authOptions);
+  adapter: PrismaAdapter(db),
+});
